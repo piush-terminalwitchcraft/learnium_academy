dcf6847009bfec37a4b0f51b2f18f01696e979a8
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2023-11-11 02:49:11 +0530

    Landing page prototype completed

--- Components/Elements/Header.tsx
+++ Components/Elements/Header.tsx
@@ -1,19 +1,56 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 import Image from 'next/image'
+import { Button } from '.';
 
 export default function Header() {
 
-  const [isMobile, setIsMobile] = React.useState(false); 
+  const [isOpen, setIsOpen] = useState(false); 
+
+  const [isMobile, setIsMobile] = useState(false)
+  
+  //choose the screen size 
+  const handleResize = () => {
+    if (window.innerWidth <= 600) {
+        setIsMobile(true)
+    } else {
+        setIsMobile(false)
+    }
+  }
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+    window.addEventListener('resize', ()=> {
+      console.log(window.innerHeight, window.innerWidth)
+  })
+  })
+
 
   return (
     <nav className='header'>
-      <div>
-        <Image src={'/logo_transparent.png'} height={100} width={100} alt='logo-image' />
+      <div id='header-mobile' onClick={()=>{setIsOpen(!isOpen)}}>
+        {
+          isOpen ?
+            <div><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64ccc5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 4L20 20M20 4L4 20" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div>
+            :
+            <div><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64ccc5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 10H21M3 14H21M3 18H21M3 6H21" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div> 
+        }
       </div>
+      <div>
+        {
+          // check if width is less than 600 px
+
+          !!isMobile ? 
+          <div style={{"fontSize":"2rem", paddingBottom:"0px"}}>Learnium</div> 
+          : 
+          <Image src={'/logo_transparent.png'} height={100} width={100} alt='logo-image' />
+        }
+      </div>
+      { !!isMobile ? <Button style={{marginLeft:"60px", backgroundColor:"var(--background)"}} >Login</Button> : null}
       <ul id='header-list'
-      className={isMobile ? 'header-list-mobile' : ''}
+      className={isOpen ? 'header-list-mobile' : ''}
       >
         <li>Home</li>
         <li>Courses</li>
@@ -21,13 +58,6 @@ export default function Header() {
         <li>Contact us</li>
         <li>Login</li>
       </ul>
-      <div id='header-mobile' onClick={()=>{setIsMobile(!isMobile)}}>
-        {
-          isMobile ?
-          <div ><svg viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64ccc5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3 21.32L21 3.32001" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /> <path d="M3 3.32001L21 21.32" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div> :
-          <div ><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64CCC5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 7L4 7" stroke="" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 12L4 12" stroke="" stroke-width="1.5" stroke-linecap="round"></path> <path d="M20 17L4 17" stroke="" stroke-width="1.5" stroke-linecap="round"></path> </g></svg></div>
-        }
-      </div>
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,73 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-12">
       <script src="float-panel.js"></script>
-      <div className='flex justify-between'>
+      <div className='flex justify-between' id='main-header-mobile'>
         <div>
           <div className='main-header'>
-          Learnium academy,<br/> the best coaching to crack <br/> JEE & NEET <br/>  
-          <span className='italic underlined'>seamlessely, with ease </span> 
+            Learnium academy,<br /> the best coaching to crack <br /> JEE & NEET <br />
+            <span className='italic underlined'>seamlessely, with ease </span>
           </div>
-          <div className='main-counselling-form flex'>
-            <div>Attend our free counselling now </div> 
-            <CustomInput type='email' placeholder='Enter your phone number'/>
-            <Button>{'>'}</Button>
+          <div className='main-counselling-form'>
+            <div>Attend free counselling now </div>
+            <div className='flex justify-start'>
+
+              <CustomInput type='email' placeholder='Enter your phone number ' style={{ "height": "40px" }} />
+              <Button style={{ "height": "40px", "margin": "0 8px" }}><svg style={{ "height": "30px" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64ccc5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.7639 12H10.0556M3 8.00003H5.5M4 12H5.5M4.5 16H5.5M9.96153 12.4896L9.07002 15.4486C8.73252 16.5688 8.56376 17.1289 8.70734 17.4633C8.83199 17.7537 9.08656 17.9681 9.39391 18.0415C9.74792 18.1261 10.2711 17.8645 11.3175 17.3413L19.1378 13.4311C20.059 12.9705 20.5197 12.7402 20.6675 12.4285C20.7961 12.1573 20.7961 11.8427 20.6675 11.5715C20.5197 11.2598 20.059 11.0295 19.1378 10.5689L11.3068 6.65342C10.2633 6.13168 9.74156 5.87081 9.38789 5.95502C9.0808 6.02815 8.82627 6.24198 8.70128 6.53184C8.55731 6.86569 8.72427 7.42461 9.05819 8.54246L9.96261 11.5701C10.0137 11.7411 10.0392 11.8266 10.0493 11.9137C10.0583 11.991 10.0582 12.069 10.049 12.1463C10.0387 12.2334 10.013 12.3188 9.96153 12.4896Z" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></Button>
+            </div>
           </div>
         </div>
-        <Image src={'/teachwwwwwwwwwwwwwwwer.png'} height={300} width={300} alt='logo-image' />
+        <video autoPlay={true} muted >
+          <source src="/video.mp4" type="video/mp4" />
+        </video>
+        {/* <Image src={'/teachwwwwwwwwwwwwwwwer.png'} height={300} width={300} alt='logo-image' /> */}
       </div>
-      <div className='main-subheader'>About our team </div>
-      <video autoPlay={true} muted>
-        <source src="/video.mp4" type="video/mp4" />
-      </video>
       <div className='main-subheader'>Courses</div>
-      <div className='slid'>JEE Courses</div>
+      <div className='main-course left'>
+        <div className='content'>
+          <div className='title'>JEE Course</div>
+          <div  className='item'>Courses and features for class 11</div>
+          <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          <div className='item'>Courses and features for class 12</div>
+          <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          
+        </div>
+      </div>
+      <div className='main-course right'>
+        <div className='content'>
+          <div  className='title'>NEET Course</div>
+          <div  className='item'>Courses and features for class 11</div>
+          <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          <div className='item'>Courses and features for class 12</div>
+          <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          
+        </div>
+      </div>
+      <div className='main-course'>
+        <div className='content'>
+          <div  className='title'>Foundation</div>
+          <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          
+        </div>
+      </div>
+
+      <div className='main-subheader'>Take LSAT</div>
+      <div className='main-lsat-form'>
+        <div>Give LSAT exam now</div>
+        <div className='flex justify-center'>
+
+          <CustomInput type='email' placeholder='Enter your phone number ' style={{ "height": "40px" }} />
+          <Button style={{ "height": "40px", "margin": "0 8px" }}><svg style={{ "height": "30px" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#64ccc5"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.7639 12H10.0556M3 8.00003H5.5M4 12H5.5M4.5 16H5.5M9.96153 12.4896L9.07002 15.4486C8.73252 16.5688 8.56376 17.1289 8.70734 17.4633C8.83199 17.7537 9.08656 17.9681 9.39391 18.0415C9.74792 18.1261 10.2711 17.8645 11.3175 17.3413L19.1378 13.4311C20.059 12.9705 20.5197 12.7402 20.6675 12.4285C20.7961 12.1573 20.7961 11.8427 20.6675 11.5715C20.5197 11.2598 20.059 11.0295 19.1378 10.5689L11.3068 6.65342C10.2633 6.13168 9.74156 5.87081 9.38789 5.95502C9.0808 6.02815 8.82627 6.24198 8.70128 6.53184C8.55731 6.86569 8.72427 7.42461 9.05819 8.54246L9.96261 11.5701C10.0137 11.7411 10.0392 11.8266 10.0493 11.9137C10.0583 11.991 10.0582 12.069 10.049 12.1463C10.0387 12.2334 10.013 12.3188 9.96153 12.4896Z" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></Button>
+        </div>
+      </div>
+
+      <div className='main-subheader'>Our Aim</div>
+      <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          
+      <div className='main-subheader'>About our team </div>
+      <div className='description'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quae praesentium hic accusamus doloribus at, iste quaerat quod ratione numquam illum accusantium, tempora sunt nesciunt cupiditate, voluptas cum harum adipisci!</div>
+          
     </main>
   )
 }
+
+
